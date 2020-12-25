071d126ee2500abecf860b087436b6233d231892
ahmedmalick <ahmedmalik2k20@gmail.com>
2020-12-25 12:36:39 +0500

    updated 3

--- src/Components/CountrySelector.js
+++ src/Components/CountrySelector.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+// import { makeStyles } from '@material-ui/core/styles';
 import FormControl from '@material-ui/core/FormControl';
 import NativeSelect from '@material-ui/core/NativeSelect';
 import { countriesData } from '../Api/api';
@@ -24,7 +24,7 @@ const CountrySelector = ({countryHandler}) => {
       <FormControl>
         <NativeSelect defaultValue="" onChange={(e)=> countryHandler(e.target.value)}>
           <option>Global</option>
-          {countriesData.map((country, i) =><option key={i} value={country}>{country}</option>)}
+          {fetchedData.map((country, i) =><option key={i} value={country}>{country}</option>)}
         </NativeSelect>
       </FormControl>
     </div>
