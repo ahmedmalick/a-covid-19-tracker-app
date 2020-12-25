5da862983e6b5aa2e797caf824b351a6e50f31cc
ahmedmalick <ahmedmalik2k20@gmail.com>
2020-12-25 12:42:57 +0500

    updated 4

--- src/Components/CountrySelector.js
+++ src/Components/CountrySelector.js
@@ -12,7 +12,7 @@ const CountrySelector = ({countryHandler}) => {
   const [fetchedData, setFetchedData] = useState([])
   useEffect(() => {
     const fetchAPI = async () => {
-      setFetchedData(await fetchedData());
+      setFetchedData(await countriesData());
     }
     fetchAPI();
   }, [setFetchedData]);
